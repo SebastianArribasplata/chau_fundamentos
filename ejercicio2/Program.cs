6eec221e054b84e0119b80699e934a61b9102ea9
UPN <UPN@ULCLAB411>
2024-11-22 12:10:42 -0500

    ejercicio2

---
+++ ejercicio2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese # Matriz:");
+            int num = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[num, num];
+            int[,] traspuestos = new int[num, num];
+            Random rnd = new Random();
+
+
+            Console.WriteLine("\n MATRIZ ORIGINAL:");
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = rnd.Next(30, 51);
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n MATRIZ TRANSPUESTA:");
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    traspuestos[j,i ] = matriz[i, j];
+
+                }
+                
+            }
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write(traspuestos[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}   
+
+
